0d48f7ca15d29e5d21fd15f85018ed836288ba10
Srinu205 <thotasrinivas946@gmail@gmail.com>
2024-04-26 14:34:11 +0530

    added new file

---
+++ smp.java
@@ -0,0 +1,13 @@
+public class Max_Of_three
+{
+int x=5;
+int y=10;
+int z=15;
+
+if (x>y && y>z)
+      System.out.println( x + " is big number ");
+else if (y>z)
+	System.out.println( y + " is big number ");
+else
+	System.out.println( z + " is big number ");
+}
